297b86f89aa019a733bfbb72752d41374bef222b
Diego Castillo <diegotony1396@gmail.com>
2023-03-23 00:22:33 -0500

    improvemnts for makefile and tilt setup

--- app.py
+++ app.py
@@ -33,7 +33,24 @@ api.add_resource(PiData, "/get-stats")
 
 @app.route("/")
 def home():
-    return render_template("index.html", name="Diego")
+    memory = psutil.virtual_memory()
+    disk = psutil.disk_usage('/')
+    system_info_data = {
+        'cpu_percent': str(psutil.cpu_percent(1)),
+        'cpu_count': str(psutil.cpu_count()),
+        'cpu_freq': str(psutil.cpu_freq()),
+        'cpu_mem_total': str(memory.total),
+        'cpu_mem_avail': str(memory.available),
+        'cpu_mem_used': str(memory.used),
+        'cpu_mem_free': str(memory.free),
+        'disk_usage_total': str(disk.total),
+        'disk_usage_used': str(disk.used),
+        'disk_usage_free': str(disk.free),
+        'disk_usage_percent': str(disk.percent),
+        'sensor_temperatures': str(psutil.sensors_temperatures()),
+        'sensor_fans': str(psutil.sensors_fans())
+    }
+    return render_template("index.html", title="rpi-stats", data=system_info_data)
 
 
 if __name__ == '__main__':
