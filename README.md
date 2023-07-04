f89dae8f19612898a6625461840f48519fdb03ca
Diego Castillo <diegotony1396@gmail.com>
2023-07-03 20:56:51 -0500

    new release

--- README.md
+++ README.md
@@ -1,5 +1,82 @@
 # rpi4-dashboard-stats
-## Resources
-- https://www.juniper.net/documentation/us/en/software/junos/automation-scripting/topics/task/junos-python-modules-psutil-module.html
-- https://www.pythonparatodo.com/?p=236
-- https://www.geeksforgeeks.org/psutil-module-in-python/
+API REST to Get stats with psutil
+## Run
+
+```bash
+make install
+source env/bin/activate
+uvicorn main:app --reload
+```
+
+## Output
+```json
+{
+"cpu": {
+"percent": 0.0,
+"cpu_count": 12,
+"cpu_freq": {
+"current": 4199.898,
+"min": 0.0,
+"max": 0.0
+},
+"cpu_usage": 3.25
+},
+"ram": {
+"total": 11,
+"used": 3,
+"available": 8
+},
+"storage": {
+"/mnt/wsl": {
+"total": 5,
+"used": 0,
+"free": 5
+},
+"/usr/lib/wsl/drivers": {
+"total": 930,
+"used": 347,
+"free": 583
+},
+"/usr/lib/wsl/lib": {
+"total": 5,
+"used": 0,
+"free": 5
+},
+"/": {
+"total": 250,
+"used": 32,
+"free": 205
+},
+"/sys/fs/cgroup/blkio": {
+"total": 0,
+"used": 0,
+"free": 0
+},
+"/sys/fs/cgroup/memory": {
+"total": 0,
+"used": 0,
+"free": 0
+},
+"/mnt/wslg/versions.txt": {
+"total": 5,
+"used": 0,
+"free": 5
+},
+"/mnt/c": {
+"total": 930,
+"used": 347,
+"free": 583
+},
+"/mnt/d": {
+"total": 931,
+"used": 510,
+"free": 420
+},
+"/Docker/host": {
+"total": 930,
+"used": 347,
+"free": 583
+}
+}
+}
+```
--- main.py
+++ main.py
@@ -1,48 +1,12 @@
-from reactpy import component, html
-from reactpy.backend.fastapi import configure
 from fastapi import FastAPI
 import system_info as si
 import utils as util
 
-@component
-def HomePage():
-    # return html.h1("Hello, world!")
-    return html.div(
-        html.p("IS LINUX? "+str(si.System_info.get_info()["IS_LINUX"])),
-        html.table(
-            html.tr(
-            html.th("            "),
-            html.th("USED"),
-            html.th("AVAILABLE"),
-            html.th("TOTAL")
-            
-            ),
-            html.tr(
-                html.td("MEMORY RAM"),
-                html.td(si.System_info.get_memory_usage()["used"]),
-                html.td(si.System_info.get_memory_usage()["available"]),
-                html.td(si.System_info.get_memory_usage()["total"])
-            )
-        ),
-        html.table(
-            html.tr(
-            html.th(""),
-            html.th("PERCENT"),
-            html.th("USAGE"),
-            html.th("COUNT"),
-            # html.th("MIN")
-            
-            ),
-            html.tr(
-                html.td("CPU"),
-                html.td(si.System_info.get_cpu_usage()["percent"]),
-                html.td(si.System_info.get_cpu_usage()["cpu_usage"]),
-                html.td(si.System_info.get_cpu_usage()["cpu_count"]),
-                # html.td(si.System_info.get_cpu_usage()["min"])
-            ),
-        )
-    )
-
-
 app = FastAPI()
-configure(app, HomePage)
+
+
+@app.get("/")
+async def root():
+    return {"cpu":  si.System_info.get_cpu_usage(), "ram": si.System_info.get_memory_usage(), "storage": si.System_info.get_disk_usage()}
+
+
